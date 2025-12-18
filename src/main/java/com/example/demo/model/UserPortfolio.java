4b53f79c42dcf4f9ca88a079f2656ead35268b45
bala-amypo <bala@amypo.in>
2025-12-18 10:02:22 +0000

    User commit

--- src/main/java/com/example/demo/model/UserPortfolio.java
+++ src/main/java/com/example/demo/model/UserPortfolio.java
@@ -31,22 +31,34 @@ public class UserPortfolio {
     public Long getUserid() { 
         return userid; 
         }
-    public void setUserid(Long userid) { this.userid = userid; }
+    public void setUserid(Long userid) { 
+        this.userid = userid; 
+        }
 
-    public String getPortfolioName() { return portfolioName; }
-    public void setPortfolioName(String portfolioName) { this.portfolioName = portfolioName; }
+    public String getPortfolioName() { 
+    return portfolioName; 
+    }
+    public void setPortfolioName(String portfolioName) {
+         this.portfolioName = portfolioName; 
+         }
 
-    public Boolean getActive() { return active; }
-    public void setActive(Boolean active) { this.active = active; }
+    public Boolean getActive() {
+         return active; 
+         }
+    public void setActive(Boolean active) { 
+        this.active = active; 
+        }
 
-    public Timestamp getCreatedAt() { return createdAt; }
-    public Timestamp getUpdatedAt() { return updatedAt; }
+    public Timestamp getCreatedAt() { 
+        return createdAt; }
 
-    // ===== DEFAULT CONSTRUCTOR =====
+    public Timestamp getUpdatedAt() { 
+        return updatedAt; }
+
     public UserPortfolio() {
     }
 
-    // ===== PARAMETERIZED CONSTRUCTOR =====
+
     public UserPortfolio(Long userid, String portfolioName) {
         this.userid = userid;
         this.portfolioName = portfolioName;
