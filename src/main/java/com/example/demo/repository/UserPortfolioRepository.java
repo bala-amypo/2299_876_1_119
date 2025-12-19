2a4ecb0aecd212e2b9717b32ae544c8c9a6891c3
bala-amypo <bala@amypo.in>
2025-12-19 05:36:46 +0000

    User commit

--- src/main/java/com/example/demo/repository/UserPortfolioRepository.java
+++ src/main/java/com/example/demo/repository/UserPortfolioRepository.java
@@ -2,9 +2,8 @@ package com.example.demo.repository;
 
 import com.example.demo.model.UserPortfolio;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.List;
 
 public interface UserPortfolioRepository extends JpaRepository<UserPortfolio, Long> {
-    List<UserPortfolio> findByUserId(Long userId);
+    List<UserPortfolio> findByUserid(Long userid);  // lowercase matches entity field
 }
