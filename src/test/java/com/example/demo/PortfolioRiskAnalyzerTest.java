0da573ee3f93b24b484829625fa3971330a3214a
bala-amypo <bala@amypo.in>
2025-12-24 07:43:42 +0000

    User commit

---
+++ src/test/java/com/example/demo/PortfolioRiskAnalyzerTest.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.testng.annotations.Test;
+import static org.testng.Assert.assertTrue;
+
+public class PortfolioRiskAnalyzerTest {
+
+    @Test
+    public void contextLoads() {
+        assertTrue(true);
+    }
+}
